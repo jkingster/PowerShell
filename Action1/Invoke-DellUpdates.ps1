9a97036c36503f8abb6e8c8a8cb17c24ef1f25d1
jkingster <jkingster@protonmail.com>
2025-02-03 17:23:16 -0600

    Updated script for proper return codes

--- Action1/Invoke-DellUpdates.ps1
+++ Action1/Invoke-DellUpdates.ps1
@@ -7,13 +7,13 @@ Function Invoke-DellUpdates {
         $computerSystem = Get-WmiObject Win32_ComputerSystem
         if ($computerSystem.Manufacturer -notlike "*Dell*") {
             Write-Host -ForegroundColor Red "Computer is not a Dell system. Exiting."
-            return
+            return -1
         }
 
         $dcuPath = "${env:ProgramFiles}\Dell\CommandUpdate\dcu-cli.exe" # Find Dell Command Update CLI
         if (-Not (Test-Path -Path $dcuPath)) {
             Write-Host -ForegroundColor Red "Computer does not have Dell Command Update installed."
-            return
+            return -1
         }
 
         $dellLogPath = "C:\Dell\Logs"
@@ -28,18 +28,24 @@ Function Invoke-DellUpdates {
     
         if ($process.ExitCode -ne 0) {
             Write-Host -ForegroundColor Red "Scan failed to complete."
-            return
+            return -1
         }
     
         $updateProcess = Start-Process -FilePath $dcuPath -ArgumentList "/applyupdates -outputLog=$dellLogPath\dcu-cli.log" -Wait -PassThru
         if ($updateProcess.ExitCode -eq 0) {
             Write-Host -Foreground Green "Dell Command Update Completed."
+            return 0
         } elseif ($updateProcess.ExitCode -eq 5) {
             Write-Host -ForegroundColor Yellow "No dell updates available."
+            return 0
         } else {
             Write-Host -ForegroundColor Red "Dell update failed."
+            return -1
         }
     } catch {
         Write-Host "Error running dell update: $_"
+        return -1
     }
 }
+
+Invoke-DellUpdates
